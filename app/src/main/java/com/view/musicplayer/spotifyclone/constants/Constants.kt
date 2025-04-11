d8b03514eb02e6688ad3f902a56eba9662345423
Mikkel <mikkelseptiano86@gmail.com>
2025-04-11 16:49:00 +0700

    Update Constants.kt

--- app/src/main/java/com/view/musicplayer/spotifyclone/constants/Constants.kt
+++ app/src/main/java/com/view/musicplayer/spotifyclone/constants/Constants.kt
@@ -1,5 +1,5 @@
 package com.view.musicplayer.spotifyclone.constants
 
 object Constants {
-    const val BASE_URL = "https://raw.githubusercontent.com/mikkelofficial7/android-spotify-clone/refs/heads/testing/"
+    const val BASE_URL = "https://raw.githubusercontent.com/mikkelofficial7/android-spotify-clone/refs/heads/main/"
 }
