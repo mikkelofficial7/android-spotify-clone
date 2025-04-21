502c678241ed32f01c7aa733b9cb63ed347540cf
Mikkel <mikkelseptiano86@gmail.com>
2025-04-21 15:09:38 +0700

    add first loader page

--- app/src/main/java/com/view/musicplayer/spotifyclone/MainActivity.kt
+++ app/src/main/java/com/view/musicplayer/spotifyclone/MainActivity.kt
@@ -7,8 +7,11 @@ import android.content.IntentFilter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -36,6 +39,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.asFlow
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
@@ -90,22 +94,14 @@ class MainActivity : ComponentActivity() {
                     val isFinishLoad by viewModel.finishLoad.observeAsState()
 
                     Box {
-                        MainPage(
-                            viewModel = viewModel,
-                            listener = notificationListener,
-                            context = this@MainActivity
-                        )
-                        if (isFinishLoad != true) {
-                            Box(
-                                contentAlignment = Alignment.Center,
-                                modifier = Modifier.fillMaxSize()
-                            ) {
-                                CircularProgressIndicator(
-                                    color = SpotifyAccent80,
-                                    strokeWidth = 4.dp,
-                                    modifier = Modifier.size(48.dp)
-                                )
-                            }
+                        if (isFinishLoad == true) {
+                            MainPage(
+                                viewModel = viewModel,
+                                listener = notificationListener,
+                                context = this@MainActivity
+                            )
+                        } else {
+                            FirstLoadingPage()
                         }
                     }
                 }
@@ -138,6 +134,31 @@ class MainActivity : ComponentActivity() {
         }
 
         LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, IntentFilter(MusicService.Notification.BROADCAST_NAME))
+    }
+}
+
+@Composable
+fun FirstLoadingPage() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Black80)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CircularProgressIndicator(
+                color = SpotifyAccent80,
+                strokeWidth = 4.dp,
+                modifier = Modifier.size(48.dp)
+            )
+            Text(
+                text = LocalContext.current.getString(R.string.loading_text),
+                color = SpotifyAccent80,
+                modifier = Modifier.padding(top = 10.dp)
+            )
+        }
     }
 }
 
